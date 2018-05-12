3f411350c93157d0c7a62b36d22a3223834e1b8c
mstieger <mstieger@192.168.1.46>
2018-05-12 08:21:44 +0200

    Small changes

--- src/communication/test/BroadcastTest.java
+++ src/communication/test/BroadcastTest.java
@@ -1,39 +1,55 @@
 package communication.test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.SocketException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 import communication.BroadcastServer;
 import communication.ServerObserver;
 import communication.messages.BroadcastMessage;
 import communication.messages.Message;
+import entity.Model;
 import entity.test.ModelTest;
 
 public class BroadcastTest implements ServerObserver{
 	private BroadcastServer broadcast;
+	private Timer clock;
+	private Model data;
 	
 	public static void main(String[] args) {
 		BroadcastTest t = new BroadcastTest();
 	}
 	
 	public BroadcastTest() {
-		try {
-			broadcast = new BroadcastServer(new ModelTest());
-			
-			broadcast.addObserver(this);
-			
+			try {
+				broadcast = new BroadcastServer();
+			} catch (SecurityException | IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}			
+			broadcast.addObserver(this);			
 			new Thread(broadcast).start();
 			
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			clock = new Timer(2000, new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					broadcast.send(null);
+					
+				}				
+			});
+			clock.start();
 	}
 
 	@Override
 	public void receive(Message msg) {
 		if(msg instanceof BroadcastMessage) {
 			BroadcastMessage bmsg = (BroadcastMessage) msg;
-			System.out.println(bmsg.toString());
 		}
 	}
 	
