bc6b67568cdfe16be98205c6b5d29cc57dfefe41
mstieger <mstieger@192.168.1.46>
2018-05-12 08:18:58 +0200

    Added members

--- src/entity/Model.java
+++ src/entity/Model.java
@@ -1,17 +1,55 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import communication.messages.Message;
+
 public class Model {
-	private InternalUser myUser;
+	private User myUser;
+	private Map<String, User> users;
+	private Map<User, LinkedBlockingQueue<Message>> messageBuffer;
+	private List<Message> chat;
 	
 	public Model() {
+		setUsers(new HashMap<>());
+		setMessageBuffer(new HashMap<>());
+		setChat(new ArrayList<>());
 	}
 
-	public InternalUser getMyUser() {
+	public User getMyUser() {
 		return myUser;
 	}
 
-	public void setMyUser(InternalUser myUser) {
+	public void setMyUser(User myUser) {
 		this.myUser = myUser;
+	}
+
+	public Map<String, User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Map<String, User> users) {
+		this.users = users;
+	}
+
+	public Map<User, LinkedBlockingQueue<Message>> getMessageBuffer() {
+		return messageBuffer;
+	}
+
+	public void setMessageBuffer(Map<User, LinkedBlockingQueue<Message>> messageBuffer) {
+		this.messageBuffer = messageBuffer;
+	}
+
+	public List<Message> getChat() {
+		return chat;
+	}
+
+	public void setChat(List<Message> chat) {
+		this.chat = chat;
 	}
 	
 }
--- src/entity/test/ModelTest.java
+++ src/entity/test/ModelTest.java
@@ -1,10 +1,10 @@
 package entity.test;
 
-import entity.InternalUser;
 import entity.Model;
+import entity.User;
 
 public class ModelTest extends Model{
 	public ModelTest() {
-		this.setMyUser(new InternalUser(0, "Test"));
+		
 	}
 }
