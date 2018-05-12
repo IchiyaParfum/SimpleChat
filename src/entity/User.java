33b39ea542186326bbdbf0dcb13b697dc194a5bc
mstieger <mstieger@192.168.1.46>
2018-05-12 08:18:04 +0200

    Changed members
    User is represented by user name and address

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,22 +1,61 @@
 package entity;
 
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class User implements Serializable{
-	protected int id;
 	protected String name;
+	protected String host;
+	protected int port;
 	
-	public User(int id, String name) {
-		this.id = id;
+	public User(String name, String host, int port) {
+		setName(name);
+		setHost(host);
+		setPort(port);
+	}
+	
+	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public int getId() {
-		return id;
+	public String getName() {
+		return name;
+	}
+	
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void setHost(String host) {
+		this.host = host;
+	}
+	
+	public String getHost() {
+		return host;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof User) {
+			User u = (User) obj;
+			if(u.getName().equals(getName()) 
+					&& u.getHost().equals(getHost())
+					&& u.getPort() == getPort()) {
+				return true;
+			}
+		}
+		return false;
+		
 	}
 	
 	@Override
 	public String toString() {
-		return "My name is " + name + " and I have id# " + id;
+		return  getName() + " " + getHost() + ":" + getPort();
 	}
 }
